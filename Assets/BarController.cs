27e830c7a1f3dddb5c29d86e13bc06ab0bac69cf
TR0926 <72895733+TR0926@users.noreply.github.com>
2020-11-08 22:30:31 +0900

    Initial commit

---
+++ Assets/BarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarController : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    //トリガーモードでほかのオブジェクトと接触したとき相手のオブジェクトを削除(課題)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
